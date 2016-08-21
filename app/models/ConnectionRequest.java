eb6331fd3138cb8df1ccf3469cb39ec9cdd13a33
Live session user <sandeep.k@betsol.com>
2016-08-21 10:52:12 +0000

    Almost Done

--- app/models/ConnectionRequest.java
+++ app/models/ConnectionRequest.java
@@ -28,4 +28,5 @@ public class ConnectionRequest extends Model{
         @EnumValue(value = "ACCEPTED")
         ACCEPTED
     }
+    public static Finder<Long, ConnectionRequest> find = new Finder<Long, ConnectionRequest>(ConnectionRequest.class);
 }
